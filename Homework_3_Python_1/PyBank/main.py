446dacb328f5bb61f608c7d1b54d3c583aede17c
Charlie Fortuno <c42know@gmail.com>
2018-03-09 11:28:55 -0800

    added python hw 1

---
+++ Homework_3_Python_1/PyBank/main.py
@@ -0,0 +1,84 @@
+import os 
+import csv
+csvpath = os.path.join('raw_data','budget_data_1.csv')
+
+#lists to store data
+months = []
+revenue = []
+rev_change = []
+date = []
+
+
+with open(csvpath, newline='') as csvfile:
+
+    #csv reader specified delimiter and variable that holds content
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+
+    for row in csvreader:
+        #add months
+        months.append(row[0])
+        revenue.append(row[1])
+        
+
+#count number of months
+num_months = len(months)
+#print(num_months)
+
+#sum revenue gained
+total_revenue =sum(map(float,revenue))
+
+#create function to turn into currency format
+def as_currency(value):
+    if value >= 0:
+        return '${:,.0f}'.format(value)
+    else:
+        return '(${:,.0f})'.format(-value)
+
+#OTHER CALCULATIONS
+map(float,revenue)
+map(float,rev_change)
+
+for i in range(1,len(revenue)):
+    rev_change.append(float(revenue[i]) - float(revenue[i-1]))
+    sum_rev_change = sum(map(float,rev_change))
+    avg_rev_change = (float(sum_rev_change)) / len(rev_change)
+
+    max_rev_change = max(rev_change)
+    min_rev_change = min(rev_change)
+
+#TEST MAX AND MIN
+# print(str(max_rev_change) + " ," + str(min_rev_change))
+
+#FIND DATE OF MAX AND MIN
+ #   max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+ #   min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+
+#GREATEST DECREASE IN REVENUE
+print("----------------------------------")
+print("Financial Analysis")
+print("----------------------------------")
+print("Total Months:" + str(num_months))
+print("Total Revenue: " + str(as_currency(total_revenue)))
+print("Average Revenue Change: " + str(as_currency(avg_rev_change)))
+#print("Greatest Increase in Revenue: " + max_rev_change_date + " " + str(as_currency(max_rev_change)))
+#print("Greatest Decrease in Revenue: " + min_rev_change_date + " " + str(as_currency(min_rev_change))
+
+# Specify the file to write to
+# output_path = os.path.join('output', 'Financial Analysis.txt')
+
+# Open the file using "write" mode. 
+# f = open(output_path,'w')
+
+# #write to the text file
+# f.write("----------------------------------\n")
+# f.write("Financial Analysis\n")
+# f.write("----------------------------------\n")
+# f.write("Total Months:" + str(num_months) + "\n")
+# f.write("Total Revenue: " + str(as_currency(total_revenue)) + "\n")
+# f.write("Average Revenue Change: " + str(as_currency(avg_rev_change)))
+# f.close
+
+
+
+
